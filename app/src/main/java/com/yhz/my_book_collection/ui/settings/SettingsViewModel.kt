f389f578294157b810b74f20be29bf14a015dbb6
Z'isalway-zzz <superman@gmail.com>
2025-09-13 18:53:11 +0700

    hehe book collection

---
+++ app/src/main/java/com/yhz/my_book_collection/ui/settings/SettingsViewModel.kt
@@ -0,0 +1,13 @@
+package com.yhz.my_book_collection.ui.settings
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is settings Fragment"
+    }
+    val text: LiveData<String> = _text
+}
